7af85382c7530ae7ef9687360b22706e742165dd
HyperTem <lukeasfilms@gmail.com>
2025-03-12 19:08:51 -0400

    Added second placement option & 1080p download.

--- README.md
+++ README.md
@@ -3,14 +3,20 @@ Mod for the Unofficial Homestuck Collection that adds the fan animation, Rex Duo
 
 By default, the animation is added after page 3763, right before entering Doc Scratch's apartment. This is the location recommended by the mod author.
 
+Also included is the option to add the animation after page 2589, right when the fight is briefly shown in the normal comic. As the original mod was never updated with more options, I took the liberty of doing so myself. This option loses out on some context (notably the appearance of the ancestors as well as some visual callbacks), but also places the animation where it canonically takes place in the comic. It is up to you which location you prefer.<br /><br />
+
 This fan animation was not in any way made by me. The animation itself contains credits for everyone who worked on it, even when viewed inside of the comic.
 
 By default, you can find a link to the original video on the flash page (I've used previously unused page 2399), however this can be disabled in the settings.
 
 You can also find a link [here](https://www.youtube.com/watch?v=-19Up0dLzNw). 
 
-If you have any suggestions for alternate places to put the flash, please create an 'issue' on the github repository.
+If you have any suggestions for alternate places to put the flash, please create an 'issue' on the original Github repository, not this fork of it. I only used my pedestrian knowledge of coding to piece together a second option for where the flash is placed. I don't actually know how to implement anything more complicated than editing what was already here, so I won't be adding anything more than this. Sorry! 
 
-## Installation
+## Installation (Recommended)
+To install, download the latest release from [here](https://drive.google.com/file/d/1YDqGuM3LDrNJCHF9ihjgydsxnhckPyTe/view?usp=sharing), and put the `rda` *folder* in your mods directory.
+This download includes RDA in full 1080p HD.
+
+## Installation (Alternate)
 To install, download the latest release from the `Releases` sidebar, and put the `rda` *folder* in your mods directory.
-
+Due to Github restrictions, this download only includes a noticeably compressed version of RDA.
--- rda/mod.js
+++ rda/mod.js
@@ -3,16 +3,17 @@ let store = null
 module.exports = {
     title: "Rex Duodecim Angelus",
     summary: "Adds Rex Duodecim Angelus to the comic.",
-    author: "Ariah",
-    modVersion: 2.0,
+    author: "Ariah, edited by HyperTem",
+    modVersion: 2.1,
 
-    description: `Adds the popular fan animation, Rex Duodecim Angelus, to the comic at a location chosen in the settings (only one location has been implemented).<br /><br />
-    By default, the animation is added after page 3763, right before entering Doc Scratch's apartment. This is the location recommended by the mod author.<br /><br />
+    description: `Adds the popular fan animation, Rex Duodecim Angelus, to the comic at a location chosen in the settings (two locations have been implemented).<br /><br />
+    By default, the animation is added after page 3763, right before entering Doc Scratch's apartment. This is the location recommended by the original mod author.<br /><br />
+	Also included is the option to add the animation after page 2589, right when the fight is briefly shown in the normal comic. As the original mod was never updated with more options, I took the liberty of doing so myself. This option loses out on some context (notably the appearance of the ancestors as well as some visual callbacks), but also places the animation where it canonically takes place in the comic. It is up to you which location you prefer.<br /><br />
     This fan animation was not in any way made by me. The animation itself contains credits for everyone who worked on it, even when viewed inside of the comic.<br /><br />
     By default, you can find a link to the original video on the flash page (I've used previously unused page 2399), however this can be disabled below.<br /><br />
     You can also find a link <a href="https://www.youtube.com/watch?v=-19Up0dLzNw">here</a>.<br /><br />
     Unfortunately, the way the the UHC supports mods currently doesn't allow me to utilize 'New Reader Mode' effectively, so new readers please refrain from viewing this flash from the homepage when it first unlocks. The page I use technically is no longer recognized as a spoiler much before you should watch the flash, but this consequently allows any of the locations in the settings to function properly in 'New Reader Mode'.<br /><br />
-    If you have any suggestions for alternate places to put the flash, please either create an 'issue' on the github repository.<br /><br />
+    If you have any suggestions for alternate places to put the flash, please create an 'issue' on the original Github repository, not this fork of it. I only used my pedestrian knowledge of coding to piece together a second option for where the flash is placed. I don't actually know how to implement anything more complicated than editing what was already here, so I won't be adding anything more than this. Sorry!<br /><br />
     <h1>Settings</h1>`,
 
     settings: {
@@ -41,15 +42,17 @@ module.exports = {
                 desc: `Put the flash right before entering Doc Scratch's apartment.<br />
                 This also adds some lines of dialogue for Doc Scratch on page 3763 and on the flash's page.<br />
                 Credit to 'TheStrifeisRife' for this dialog.`
-            }/*,{
+            },{
                 value: {
-                    content: "Wow we have no context for this.",
-                    previous: '001901',
-                    next: '001902'
+                    title: "[S] As you were, then.",
+					content: "",
+                    previous: '004489',
+                    next: '004490'
                 },
-                label: "After Page ????",
-                desc: `Put the flash somewhere else.`
-            }*/]
+                label: "After Page 2589",
+                desc: `Put the flash at the point where Aradia explains the Trolls' fight with the Black King.<br />
+				This way, the video serves as a proper climax for Act 5 Act 1. You'll miss out on some context for the flash, but in turn, other pages will call back to it later in the comic. This may make it feel like a more or less authentic part of the real comic, depending on your perspective.`
+            }]
         }]
     },
 
@@ -70,7 +73,7 @@ module.exports = {
             thumbsrc: "/archive/music/alterniabound/rex-duodecim-angelus.jpg",
             date: "",
             title: '[S] Rex Duodecim Angelus',
-            description: `<p>The popular fan animation Rex Duodecim Angelus.<br />Don't watch before at least finishing Act 5 Act 1, if not later.</p>`
+            description: `<p>The popular fan animation which depicts the Trolls' fight with the Black King in its entirety.<br />Don't watch before at least finishing Act 5 Act 1, if not later.</p>`
         })
 
         archive.music.flashes['004299'] = {
